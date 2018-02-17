387c666452c6a7fe90bfd3d4946ffea621c1ecfd
tanakmura <tanakmura@gmail.com>
2018-02-17 10:59:49 +0900

    add storeforwarding test

--- bench.cpp
+++ bench.cpp
@@ -714,6 +714,14 @@ main(int argc, char **argv)
     GEN(Reg64, "load", (g->mov(dst, g->ptr[src + g->rdx])), false, OT_INT);
     GEN(Reg64, "crc32", (g->crc32(dst, src)), false, OT_INT);
 
+    GEN(Reg64, "store [mem+0]->load[mem+0]", 
+        (g->mov(g->ptr[src+g->rdx],g->rdi)) ; (g->mov(dst, g->ptr[g->rdx])),
+        false, OT_INT);
+
+    GEN(Reg64, "store [mem+0]->load[mem+1]", 
+        (g->mov(g->ptr[src+g->rdx],g->rdi)) ; (g->mov(dst, g->ptr[g->rdx + 1])),
+        false, OT_INT);
+
     GEN(Xmm, "pxor", (g->pxor(dst, src)), false, OT_INT);
     GEN(Xmm, "padd", (g->paddd(dst, src)), false, OT_INT);
     GEN(Xmm, "pmuldq", (g->pmuldq(dst, src)), false, OT_INT);
